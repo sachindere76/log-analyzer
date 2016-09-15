4d607e21489a170f500f0b2e910d9cef93909e7a
Joerg Eschmann <joerg.eschmann@gmail.com>
2016-09-15 07:09:01 +0200

    use java.io.tmpdir to store output files

--- src/test/java/com/joergeschmann/tools/loganalyzer/AppTest.java
+++ src/test/java/com/joergeschmann/tools/loganalyzer/AppTest.java
@@ -1,8 +1,10 @@
 package com.joergeschmann.tools.loganalyzer;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
@@ -11,11 +13,18 @@ public class AppTest {
     public void run() {
 
         URL logFileUrl = URLClassLoader.getSystemResource("logfiles/small.log");
+        File outputFile = new File(System.getProperty("java.io.tmpdir"), "log-analyer-result.txt");
+        
+        if(outputFile.exists()) {
+        	outputFile.delete();
+        }
+        
+        Assert.assertFalse(outputFile.exists());
 
         String[] arguments = new String[] { // no line break
 
                 "--logFile", logFileUrl.getPath(), // no line break
-                "--outputFile", "/tmp/result.txt", // no line break
+                "--outputFile", outputFile.getAbsolutePath(), // no line break
                 "--exactDateFilter", "2016-06-15", // no line break
                 "--timeRangeFilter", "23:58:00", "23:59:00", // no line break
                 "--valueFilter", "-n", "component", "Class1", "valueFilter", "-i", "message",
@@ -25,6 +34,8 @@ public class AppTest {
 
         App app = new App(arguments);
         app.run();
+        
+        Assert.assertTrue(outputFile.exists());
 
     }
 
